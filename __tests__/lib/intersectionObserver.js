6ef6569f7c1d8863205f80c2cad8e652cd78716e
Guillaume <gui.lebas@gmail.com>
2019-12-02 11:23:43 +0100

    fix(__tests__/lib/intersectionObserver) Fix tests after 1273144

--- __tests__/lib/intersectionObserver.js
+++ __tests__/lib/intersectionObserver.js
@@ -6,14 +6,21 @@ const callback = jest.fn((entries, observer) => callbackReturnValues.push({ entr
 const observer = new IntersectionObserver(callback)
 
 let callbackReturnValues
+
 beforeEach(() => callbackReturnValues = [])
+
+// (1) The mock of IntersectionObserver use `setTimeout(callback, 0)` to delay
+// callback and simulate a real "scroll -> delay -> intersection" sequence.
+jest.useFakeTimers()
 
 it('execute the callback when observing elements', () => {
 
+    // Act
     elements.forEach(element => observer.observe(element))
 
+    // Assert
     elements.forEach(element => expect(observer.entries.includes(element)).toBe(true))
-    expect(callback).toHaveBeenCalledTimes(3)
+    expect(callback).toHaveBeenCalledTimes(elements.length)
     callbackReturnValues.forEach(({ entries: [entry], observer: actualObserver }, index) => {
         expect(entry.isIntersecting).toBe(index === 0)
         expect(entry.target).toBe(elements[index])
@@ -22,8 +29,11 @@ it('execute the callback when observing elements', () => {
 })
 it('execute the callback after a WheelEvent', () => {
 
+    // Act
     document.dispatchEvent(new WheelEvent('wheel', { deltaY: 1 }))
+    jest.runAllTimers() /* (1) */
 
+    // Assert
     expect(callback).toHaveBeenCalledTimes(4)
     callbackReturnValues.forEach(({ entries: [entry] }) =>
         expect(entry.isIntersecting).toBe(entry.target === elements[1]))
