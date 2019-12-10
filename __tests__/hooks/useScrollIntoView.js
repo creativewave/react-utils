ab03fe75c26a2f5a2930afc707655ec8bf7ed66a
Guillaume <gui.lebas@gmail.com>
2019-12-10 12:58:02 +0100

    fix(__tests__/hooks/useScrollIntoView) Fix minor coding/comments convention issues

--- __tests__/hooks/useScrollIntoView.js
+++ __tests__/hooks/useScrollIntoView.js
@@ -10,7 +10,7 @@ let container
 beforeEach(() => {
     container = document.createElement('div')
     document.body.appendChild(container)
-    // (1) Use fake timers to throttle wheel events (and intersections)
+    // (1) Use fake timers to throttle scroll and intersections
     jest.useFakeTimers()
 })
 afterEach(() => {
@@ -64,7 +64,6 @@ const cases = [
 
         React.useEffect(
             () => {
-
                 targets.forEach(id => {
                     if (!prevTargets.current.includes(id)) {
                         prevTargets.current.push(id)
@@ -153,54 +152,73 @@ it.each(cases)('%s', (_, Test) => {
     let currentTargetIndex
     let elements
     let observer
-    let onEnterCount = 0
-    let onExitCount = 0
+    let onEnterCalls = 0
+    let onExitCalls = 0
     let nextTargetIndex
     let root
     let scrollDirection
     let targets = ['target-1', 'target-2', 'target-3', 'target-4']
 
-    // 1. It executes onOnter() or onExit() for each mounted target when mounting root
+    /**
+     * It executes onOnter() or onExit() for each mounted target when mounting
+     * root.
+     */
     act(() => {
         render(<Test config={config} targets={targets} />, container)
     })
 
-    expect(config.onEnter).toHaveBeenCalledTimes(++onEnterCount)
-    expect(config.onExit).toHaveBeenCalledTimes(onExitCount = targets.length - 1)
+    expect(config.onEnter).toHaveBeenCalledTimes(++onEnterCalls)
+    expect(config.onExit).toHaveBeenCalledTimes(onExitCalls = targets.length - 1)
 
-    // 2. It executes beforeScroll() -> scrollIntoView() -> onExit() and onOnter() after a wheel event in root
-    // Memo: the mock of IntersectionObserver triggers intersections on wheel
+    /**
+     * It executes beforeScroll() -> scrollIntoView() -> onExit() or onOnter()
+     * after a wheel event (scroll down) in root.
+     *
+     * (2) jsdom doesn't implement Element.prototype.scrollIntoView().
+     * (3) The mock of IntersectionObserver should trigger an intersection on
+     * wheel.
+     */
     act(() => {
 
         root = container.querySelector('#root') || document
         elements = container.querySelectorAll('[id^=target]')
-        // Memo: jsdom doesn't implement Element.prototype.scrollIntoView()
-        elements.forEach(element => element.scrollIntoView = jest.fn())
+        elements.forEach(target => target.scrollIntoView = jest.fn()) // (2)
 
-        root.dispatchEvent(new WheelEvent('wheel', { bubbles: true, deltaY: 1 }))
-        jest.advanceTimersByTime(config.wait) /* (1) */
+        root.dispatchEvent(new WheelEvent('wheel', { bubbles: true, deltaY: 1 })) // (3)
+        jest.advanceTimersByTime(config.wait) // (1)
         currentTargetIndex = 1
     })
 
-    expect(config.beforeScroll).toHaveBeenCalledTimes(1)
-    expect(config.beforeScroll).toHaveBeenCalledWith(1, 0, 'down')
-    expect(elements[currentTargetIndex].scrollIntoView).toHaveBeenCalledTimes(1)
-    expect(elements[currentTargetIndex].scrollIntoView).toHaveBeenCalledWith({ behavior: config.mode })
-    expect(config.onEnter).toHaveBeenCalledTimes(++onEnterCount)
-    expect(config.onExit).toHaveBeenCalledTimes(++onExitCount)
+    expect(config.beforeScroll).toHaveBeenNthCalledWith(1, 1, 0, 'down')
+    expect(elements[currentTargetIndex].scrollIntoView).toHaveBeenNthCalledWith(1, { behavior: config.mode })
+    expect(config.onEnter).toHaveBeenCalledTimes(++onEnterCalls)
+    expect(config.onExit).toHaveBeenCalledTimes(++onExitCalls)
 
-    // 3. It executes observer.observe() -> onExit() when mounting a new target
+    /**
+     * It executes observer.observe() -> onExit() when mounting a new target.
+     */
     act(() => {
         observer = observers.observers[0][0] // eslint-disable-line prefer-destructuring
         jest.spyOn(observer, 'observe')
-        targets = targets.concat('target-5')
-        render(<Test config={config} targets={targets} />, container)
+        render(<Test config={config} targets={targets = targets.concat('target-5')} />, container)
     })
 
     expect(observer.observe).toHaveBeenCalledTimes(1)
-    expect(config.onExit).toHaveBeenCalledTimes(++onExitCount)
+    expect(config.onExit).toHaveBeenCalledTimes(++onExitCalls)
 
-    // 4. It re-initializes events listeners after an update of a hook option
+    /**
+     * It re-initializes event listeners after an update of a hook option.
+     *
+     * (4) The mock of IntersectionObserver sets a scrollTop property to 0 when
+     * instantiated, such as after an update of a hook option, eventually, and
+     * use it to compute the intersecting entries and the current target on
+     * observe() -> onEnter(), but it should make sure to scroll the last target
+     * index into view, whatever the current target index is, and it should not
+     * use a static value, in order to scroll to a different index after this
+     * step.
+     * (5) The mock of IntersectionObserver adds a wheel event listener and
+     * removes it only when it's disconnected, ie. if root !== document.
+     */
     act(() => {
 
         jest.spyOn(root, 'addEventListener')
@@ -217,46 +235,52 @@ it.each(cases)('%s', (_, Test) => {
         render(<Test config={config} targets={targets} />, container)
     })
 
-    // Memo: the mock of IntersectionObserver adds a wheel event listener and
-    // removes it only when it's disconnected, ie. if root !== document
-    expect(root.removeEventListener).toHaveBeenCalledTimes(root === document ? events.length : events.length + 1)
+    expect(root.removeEventListener).toHaveBeenCalledTimes(
+        root === document // (5)
+            ? events.length
+            : events.length + 1)
 
-    // 5. It executes scrollIntoView() with the value returned by beforeScroll() after a wheel event
+    /**
+     * It executes scrollIntoView() on the target whose index is returned by
+     * beforeScroll() after a wheel event.
+     */
     act(() => {
         root.dispatchEvent(new WheelEvent('wheel', { bubbles: true, deltaY: 1 }))
-        jest.advanceTimersByTime(config.wait) /* (1) */
+        jest.advanceTimersByTime(config.wait) // (1)
     })
 
-    expect(config.beforeScroll).toHaveBeenCalledTimes(1)
-    expect(config.beforeScroll).toHaveBeenCalledWith(nextTargetIndex, currentTargetIndex, scrollDirection)
+    expect(config.beforeScroll).toHaveBeenNthCalledWith(1, nextTargetIndex, currentTargetIndex, scrollDirection)
     expect(config.beforeScroll).toHaveReturnedWith(currentTargetIndex)
-    expect(elements[currentTargetIndex].scrollIntoView).toHaveBeenCalledTimes(root === document ? 2 : 1)
-    expect(elements[currentTargetIndex].scrollIntoView).toHaveBeenCalledWith({ behavior: config.mode })
+    expect(elements[currentTargetIndex].scrollIntoView).toHaveBeenNthCalledWith(
+        root === document ? 2 : 1,
+        { behavior: config.mode })
 
     // TODO: add tests when target index < 0 or index >= targets.length
     // TODO: add tests with config.directions = 'x' and wheel events in y direction
     // TODO: add tests with config.directions = 'y' and wheel events in x direction
     // TODO: add tests with config.directions = 'both' and wheel events in both directions
 
-    // 6. It executes observer.unobserve() before a target unmounts
+    /**
+     * It executes observer.unobserve() before a target unmounts.
+     */
     act(() => {
-
         observer = observers.observers[0][0] // eslint-disable-line prefer-destructuring
         jest.spyOn(observer, 'unobserve')
-
-        targets = targets.slice(0, targets.length - 1)
-        render(<Test config={config} targets={targets} />, container)
+        render(<Test config={config} targets={targets = targets.slice(0, targets.length - 1)} />, container)
     })
 
     expect(observer.unobserve).toHaveBeenCalledTimes(1)
 
-    // 7. It removes listener and executes observer.disconnect() before root unmounts if root !== document
+    /**
+     * It removes event listeners and executes observer.disconnect() before root
+     * unmounts.
+     */
     act(() => {
         jest.spyOn(observer, 'disconnect')
         render(null, container)
     })
 
-    // Note: see memo in 4.
+    // (5)
     if (root === document) {
         expect(root.removeEventListener).toHaveBeenCalledTimes(events.length * 2)
     } else {
