77c4c329e121d1c1e401ca6d2e20e1fc6f02667f
Guillaume <gui.lebas@gmail.com>
2020-07-24 15:21:17 +0200

    fix(rollup.config) Switch to Rollup

---
+++ rollup.config.js
@@ -0,0 +1,72 @@
+
+import babel from 'rollup-plugin-babel'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import pkg from './package.json'
+import replace from '@rollup/plugin-replace'
+import { terser } from 'rollup-plugin-terser'
+
+const {
+    dependencies = {},
+    optionalDependencies = {},
+    peerDependencies = {},
+} = pkg
+
+const pattern = `^(${Object.keys({
+    ...dependencies,
+    ...optionalDependencies,
+    ...peerDependencies,
+}).concat('prop-types').join('|')})`
+
+const external = id => (new RegExp(pattern)).test(id)
+
+const replaceEnv = replace({ 'process.env.NODE_ENV': process.env.NODE_ENV })
+
+const getBabelConfig = targets => ({
+    exclude: /node_modules/,
+    presets: [
+        ['@babel/preset-env', {
+            corejs: 3,
+            // debug: true,
+            targets,
+            useBuiltIns: 'usage',
+        }],
+        '@babel/preset-react',
+    ]
+})
+
+export default [
+    {
+        external,
+        input: 'src/index.js',
+        output: {
+            file: pkg.main,
+            format: 'cjs',
+        },
+        plugins: [replaceEnv, babel(getBabelConfig({ node: true }))],
+    },
+    {
+        external,
+        input: 'src/index.js',
+        output: {
+            file: pkg.module,
+            format: 'es',
+        },
+        plugins: [replaceEnv, babel(getBabelConfig({ esmodules: true }))],
+    },
+    {
+        input: 'src/index.js',
+        output: {
+            file: pkg.unpkg,
+            format: 'iife',
+            name: 'animate',
+        },
+        plugins: [
+            replaceEnv,
+            nodeResolve(),
+            babel(getBabelConfig('defaults')),
+            commonjs(),
+            terser(),
+        ],
+    },
+]
