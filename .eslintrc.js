5458a184d607a0ebe080ff4846ad84a5278696d5
Guillaume <gui.lebas@gmail.com>
2020-07-24 15:24:03 +0200

    fix(.eslintrc) Define unsupported APIs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,18 @@
 
+// Users should polyfill themselves the following interfaces.
+const notPolyfilled = [
+    'IntersectionObserver',
+    'TouchEvent',
+    'WheelEvent',
+]
+
 module.exports = {
     env: { jest: true },
     extends: ['@cdoublev/eslint-config/react'],
+    settings: {
+        polyfills: [
+            'Object',
+            ...notPolyfilled,
+        ],
+    },
 }
