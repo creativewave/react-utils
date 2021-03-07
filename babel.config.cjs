4895a74cafbf5c958b361a5b164404aac4b09bf5
Guillaume <gui.lebas@gmail.com>
2021-03-07 11:53:03 +0100

    fix(package) Add Node CJS build

--- babel.config.cjs
+++ babel.config.cjs
@@ -13,17 +13,15 @@ module.exports = api => {
 
     const env = api.env()
 
-    if (env === 'browser') {
+    if (env === 'cjs' || env === 'es') {
+        plugins.push(['@babel/plugin-transform-runtime', { version: dependencies['@babel/runtime'] }])
+        presetEnv.modules = env === 'es' ? false : 'auto'
+    } else if (env === 'umd') {
         presetEnv.targets = { esmodules: true }
         return { exclude: /core-js/, plugins, presets }
     }
 
     presetEnv.targets = { node: true }
-
-    if (env === 'node') {
-        plugins.push(['@babel/plugin-transform-runtime', { version: dependencies['@babel/runtime'] }])
-        presetEnv.modules = false
-    }
 
     return { exclude: /node_modules/, plugins, presets }
 }
--- rollup.config.js
+++ rollup.config.js
@@ -4,26 +4,44 @@ import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import { terser } from 'rollup-plugin-terser'
-import { unpkg } from './package.json'
+import { dependencies, exports, optionalDependencies, peerDependencies, unpkg } from './package.json'
 
-export default {
-    external: ['@cdoublev/animate', 'react', 'prop-types'],
-    input: 'src/index.js',
-    output: {
-        file: unpkg,
-        format: 'umd',
-        globals: {
-            '@cdoublev/animate': 'animate',
-            'prop-types': 'PropTypes',
-            'react': 'React',
+export default process.env.NODE_ENV === 'cjs'
+    ? {
+        input: 'src/index.js',
+        external: new RegExp(`^(${
+            Object.keys({
+                ...dependencies,
+                ...optionalDependencies,
+                ...peerDependencies,
+            }).join('|')})`),
+        output: {
+            file: exports['.'].require,
+            format: 'cjs',
         },
-        name: 'ReactUtils',
-    },
-    plugins: [
-        replace({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) }),
-        nodeResolve(),
-        babel({ babelHelpers: 'bundled' }),
-        commonjs(),
-        terser(),
-    ],
-}
+        plugins: [
+            babel({ babelHelpers: 'runtime' }),
+            commonjs(),
+        ],
+    }
+    : {
+        external: ['@cdoublev/animate', 'react', 'prop-types'],
+        input: 'src/index.js',
+        output: {
+            file: unpkg,
+            format: 'umd',
+            globals: {
+                '@cdoublev/animate': 'animate',
+                'prop-types': 'PropTypes',
+                'react': 'React',
+            },
+            name: 'ReactUtils',
+        },
+        plugins: [
+            replace({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) }),
+            nodeResolve(),
+            babel({ babelHelpers: 'bundled' }),
+            commonjs(),
+            terser(),
+        ],
+    }
